a4181daa689e1d6419e193ccd047727fd6c1ef6e
Ruta <ruta@baltiejus.com>
2016-04-27 17:46:15 +0100

    adding a few gifs, completed Challenge 25

--- battle/spec/game_spec.rb
+++ battle/spec/game_spec.rb
@@ -1,14 +1,27 @@
 require 'game'
 
 describe Game do
-  let(:cersei) {double :cersei}
-  let(:tyrion) {double :tryrion}
+  subject(:game) {described_class.new(player_1, player_2)}
+  let(:player_1) {double :player}
+  let(:player_2) {double :player}
+
+  describe '#player 1' do
+    it 'retrieves first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player 2' do
+    it 'retrieves second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
 
   describe 'attack' do
 
     it 'damages the player' do
       expect(subject).to receive(:attack)
-      subject.attack(cersei)
+      subject.attack(player_2)
     end
   end
 end
