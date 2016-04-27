ba0fab1fea15d92f32a865d5969b0bcb1388a0eb
Daniel Ortiz <danielortiz@Daniels-MacBook-Pro-6.local>
2016-04-27 15:01:33 +0100

    created Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,9 @@
 require 'player'
 
-
 describe Player do
+
+  subject(:lucy) { Player.new('lucy') }
+
   it "knows it's name" do
     our_player = Player.new('player')
     expect(our_player.name).to eq 'player'
