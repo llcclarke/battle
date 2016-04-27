30a69ebfd602bb1997fefc560494ab2205ad764e
Jack Hardy <jackhardy1@gmail.com>
2016-04-27 10:41:00 +0100

    hit points player 2

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -1,9 +1,19 @@
-feature 'Enter names' do
+feature 'Names' do
   scenario 'submitting names' do
     visit('/')
     fill_in :player_1_name, with: 'Jack'
     fill_in :player_2_name, with: 'Ruta'
     click_button 'Submit'
     expect(page).to have_content 'Jack vs. Ruta'
+  end
+end
+
+feature 'Hit points' do
+  scenario 'see hit points for second player' do
+    visit('/')
+    fill_in :player_1_name, with: 'Jack'
+    fill_in :player_2_name, with: 'Ruta'
+    click_button 'Submit'
+    expect(page).to have_content 'Ruta: 100 HP'
   end
 end
