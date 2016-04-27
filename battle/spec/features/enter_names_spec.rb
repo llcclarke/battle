e7fd0496c1ce572126e04e2dcaff4ddfec2873a6
Jack Hardy <jackhardy1@gmail.com>
2016-04-27 10:31:51 +0100

    player names done

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -2,8 +2,8 @@ feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
     fill_in :player_1_name, with: 'Jack'
-    fill_in :player_2_name, with: 'Emilio'
+    fill_in :player_2_name, with: 'Ruta'
     click_button 'Submit'
-    expect(page).to have_content 'Jack vs. Emilio'
+    expect(page).to have_content 'Jack vs. Ruta'
   end
 end
