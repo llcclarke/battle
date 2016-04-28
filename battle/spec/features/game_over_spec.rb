4283cb219a797564579995c7239169134f05f65e
Noah Pollock <pollocknoah@gmail.com>
2016-04-29 00:53:02 +0100

    Game Over Spec Working

--- battle/spec/features/game_over_spec.rb
+++ battle/spec/features/game_over_spec.rb
@@ -1,10 +1,8 @@
 feature 'Game Over' do
-  context 'when Player 2 reaches 0 HP first' do
-    before do
-      sign_in_and_play
-      9.times { attack }
-      click_button 'Attack'
-      expect(page).to have_content 'Tyrion Lannister loses!'
-    end
+  scenario 'when Player 2 reaches 0 HP first' do
+    sign_in_and_play
+    18.times { attack }
+    click_button 'Attack'
+    expect(page).to have_content 'Cersei Lannister loses!'
   end
 end
